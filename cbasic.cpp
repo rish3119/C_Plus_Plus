0c2fa423b926f9c75ef463e70fc81147c746f80d
rish3119 <rishavsingh312001@gmail.com>
2026-06-07 03:07:53 -0700

    C++-BASIC

---
+++ cbasic.cpp
@@ -0,0 +1,8 @@
+#include <iostream>   // for input/output
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;  // prints text to the console
+    return 0;  // signals successful program end
+}

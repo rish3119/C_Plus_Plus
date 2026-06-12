9a009fa9d906b70e7a737f87a2dc3e5a7fda6563
Rishav kumar <65082283+rish3119@users.noreply.github.com>
2026-06-12 23:35:08 +0530

    Add files via upload

---
+++ SUM_Array.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen; // value -> index
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (seen.find(complement) != seen.end()) {
+                return {seen[complement], i}; // found pair
+            }
+            seen[nums[i]] = i; // store current number
+        }
+        return {}; // no solution (though problem guarantees one)
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> result = sol.twoSum(nums, target);
+    cout << "Indices: " << result[0] << ", " << result[1] << endl;
+
+    return 0;
+}

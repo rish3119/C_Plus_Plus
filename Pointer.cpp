65229ece2b65c637f7e901b6d38e70e9a03cd1b2
Rishav kumar <65082283+rish3119@users.noreply.github.com>
2026-06-18 22:53:00 +0530

    Add files via upload

---
+++ Pointer.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    
+    int sum = *a + *b;
+    int diff = *a - *b;
+
+    if (diff < 0) {
+        diff = -diff;  // absolute value
+    }
+
+    *a = sum;
+    *b = diff;
+
+    // Complete this function    
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
